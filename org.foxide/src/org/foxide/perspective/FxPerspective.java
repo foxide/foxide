3aeeedb844b8e17b9075e16f9be20ae9275b9b86
sashcode <sashcode@gmail.com>
2014-02-12 01:11:32 +0900

    add feature project and site project

--- org.foxide/src/org/foxide/perspective/FxPerspective.java
+++ org.foxide/src/org/foxide/perspective/FxPerspective.java
@@ -7,10 +7,10 @@ import org.eclipse.ui.IPerspectiveFactory;
 public class FxPerspective implements IPerspectiveFactory {
 
     public static final String PACKAGE_EXPLORER = "org.eclipse.jdt.ui.PackageExplorer";
-    public static String MARKERS = "org.eclipse.ui.views.AllMarkersView";
-    public static String PROJECT_EXPLORER = IPageLayout.ID_PROJECT_EXPLORER;
-    public static String OUTLINE = IPageLayout.ID_OUTLINE;
-    public static String PROPERTIES = IPageLayout.ID_PROP_SHEET;
+    public static final String MARKERS = "org.eclipse.ui.views.AllMarkersView";
+    public static final String PROJECT_EXPLORER = IPageLayout.ID_PROJECT_EXPLORER;
+    public static final String OUTLINE = IPageLayout.ID_OUTLINE;
+    public static final String PROPERTIES = IPageLayout.ID_PROP_SHEET;
 
     @Override
     public void createInitialLayout(IPageLayout factory) {
@@ -20,12 +20,10 @@ public class FxPerspective implements IPerspectiveFactory {
         IFolderLayout bottom = factory.createFolder("bottomRight", //$NON-NLS-1$
                 IPageLayout.BOTTOM, 0.75f, factory.getEditorArea());
 
-
         IFolderLayout topRight = factory.createFolder("topRight", //$NON-NLS-1$
                 IPageLayout.RIGHT, 0.75f, factory.getEditorArea());
 
-        
-        topLeft.addView(PACKAGE_EXPLORER);
+        topLeft.addView(PROJECT_EXPLORER);
         topRight.addView(OUTLINE);
         bottom.addView(MARKERS);
         bottom.addView(PROPERTIES);
